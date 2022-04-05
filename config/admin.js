742a5c177b9235d6e93b2fa40b013a48ec4588d7
lychee <lychee@rb2.nl>
2022-04-05 11:00:31 +0800

    feat: init

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '3528e3e37efeebd33e384506293bb6f1'),
+    secret: env('ADMIN_JWT_SECRET', ''),
   },
 });
--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', ''),
       port: env.int('DATABASE_PORT', 0),
-      database: env('DATABASE_NAME', 'ce'),
+      database: env('DATABASE_NAME', ''),
       user: env('DATABASE_USERNAME', ''),
       password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
